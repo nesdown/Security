927903e95a5dfe55ba746616e8f56c972daa247d
Bohdan Glushko <glushko.bohdan@gmail.com>
2020-12-12 18:54:32 +0200

    Update xor

--- Lab01/xor_cipher_cracker.py
+++ Lab01/xor_cipher_cracker.py
@@ -3,35 +3,39 @@ import operator
 import binascii
 import itertools
 
+# Простой класс, генерирующий ввсе н-битные ключи
 class XORCipherCracker():
 
-    def __init__(self, plain_text_chars):
-        self.plain_text_chars = plain_text_chars
+# В инициализаторе берем последовательность символов
+    def __init__(self, text_cyphered_symbols):
+        self.text_cyphered_symbols = text_cyphered_symbols
 
-    def crack(self, cipher_text_in_binary):
-        key_lengths_with_probabilities = self.find_likely_key_lengths(cipher_text_in_binary)
-        most_likely_key_length, probability = key_lengths_with_probabilities[0]
+    def crack(self, binary_cipher_of_text):
+        key_length_probability = self.find_likely_key_lengths(binary_cipher_of_text)
+        key_length_to_be, probability = key_length_probability[0]
 
-        # generate all possible n-bit keys
-        possible_keys = ["".join(bit) for bit in itertools.product("01", repeat=most_likely_key_length)]
+        # Какие н-битные ключи вообще могут быть?
+        possible_keys = ["".join(bit) for bit in itertools.product("01", repeat=key_length_to_be)]
 
-        keys = self.find_likely_keys(possible_keys, cipher_text_in_binary)
+        keys = self.find_likely_keys(possible_keys, binary_cipher_of_text)
 
+        # Тут будем собирать все что нарасшифровывали
         decryptions = {}
         for key in keys:
             print(str(key))
-            possible_plain_text = self.decrypt_with_key(key, cipher_text_in_binary)
-            if possible_plain_text:
-                decryptions[key] = possible_plain_text
+            text_plain_variants = self.decrypt_with_key(key, binary_cipher_of_text)
+            if text_plain_variants:
+                decryptions[key] = text_plain_variants
 
+        # Если варианты нашлись, то будем их показывать
         return decryptions
 
     def find_likely_key_lengths(self, cipher_text):
         possible_key_lengths = {}
         for key_length in range(2, 32):
             hamming_distances = position = counter = 0
-            # calculate the hamming distance between sequential pieces of ciphertext,
-            # with the size of the pieces being determined by the key length being tested.
+            # вычисляем расстояние хемминга
+            # размеры тестируются по длинне возможного ключа
             while(position + 2 * key_length <= len(cipher_text)):
                 section_one = cipher_text[position:position+key_length]
                 position += key_length
@@ -41,13 +45,13 @@ class XORCipherCracker():
                 counter += 1
             average_hamming_distance = hamming_distances / float(counter)
 
-            # normalise the average hamming distance by dividing by key length
+            # выполним нормализацию
             possible_key_lengths[key_length] = average_hamming_distance / float(key_length)
 
-        # key length with lowest normalised average hamming distance is most likely to be the correct one
+        # длина ключа с самым низким средним скорее всего наиболее подходящая
         return sorted(possible_key_lengths.items(), key=operator.itemgetter(1), reverse=False)
 
-    # https://en.wikipedia.org/wiki/Hamming_distance
+    
     def calculate_hamming_distance(self, string_one, string_two):
         count = 0.0
         for i in range(len(string_one)):
@@ -55,34 +59,38 @@ class XORCipherCracker():
                 count += 1
         return count
 
-    # find the keys that produce a plaintext containing only the legal characters
-    def find_likely_keys(self, possible_keys, cipher_text_in_binary):
+    # найдем ключи, которые возвращают только валидные значения
+    def find_likely_keys(self, possible_keys, binary_cipher_of_text):
         keys = []
         for possible_key in possible_keys:
-            if self.does_key_produce_only_legal_chars(possible_key, cipher_text_in_binary):
+            if self.does_key_produce_only_legal_chars(possible_key, binary_cipher_of_text
+    ):
                 keys.append(possible_key)
         return keys
 
-    # checks to see if the key xors each key length block of cipher text
-    # into only legal characters (passed in in the constructor)
-    def does_key_produce_only_legal_chars(self, possible_key, cipher_text_in_binary):
+    # при ксоре, длина ключа равна длине блока шифрованного текста?
+    def does_key_produce_only_legal_chars(self, possible_key, binary_cipher_of_text):
         section = 0
-        while section < len(cipher_text_in_binary):
-            portion = cipher_text_in_binary[section: section+len(possible_key)]
+        while section < len(binary_cipher_of_text
+):
+            portion = binary_cipher_of_text
+    [section: section+len(possible_key)]
             xor_result = int(possible_key, 2) ^ int(portion, 2)
             try:
-                possible_plain_text_letter = binascii.unhexlify('%x' % xor_result)
-                for char in list(possible_plain_text_letter):
-                    if char.lower() not in self.plain_text_chars:
+                text_plain_variants_letter = binascii.unhexlify('%x' % xor_result)
+                for char in list(text_plain_variants_letter):
+                    if char.lower() not in self.text_cyphered_symbols:
                         return False
             except TypeError:
                 return False
             section += len(possible_key)
         return True
 
-    def decrypt_with_key(self, key, cipher_text_in_binary):
-        repeated_key = self.repeat_key_to_length(key, len(cipher_text_in_binary))
-        return int(repeated_key, 2) ^ int(cipher_text_in_binary, 2)
+    def decrypt_with_key(self, key, binary_cipher_of_text):
+        repeated_key = self.repeat_key_to_length(key, len(binary_cipher_of_text
+))
+        return int(repeated_key, 2) ^ int(binary_cipher_of_text
+, 2)
 
     def repeat_key_to_length(self, key, cipher_text_length):
         repeat_key = (key * ((cipher_text_length / len(key)) + 1))
