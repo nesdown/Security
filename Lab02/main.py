30a31dff6a3f2d5f9f7cca8ce1a8d10f22aec7fa
Bohdan Glushko <glushko.bohdan@gmail.com>
2020-12-12 18:05:13 +0200

    Update of code
    Updated the file structure

---
+++ Lab02/main.py
@@ -0,0 +1,3 @@
+# It seems I have not written a single line of code here... The solution is right here!
+# https://docs.google.com/document/d/17ryP8MU4xX9XUg1MShhZapdXoc92ZdTBCWdlEDmsgGw/edit?usp=sharing
+
